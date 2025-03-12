b92984fa298fdcf7ba78d8352c3cc0e93e742145
Cyb3s <157359344+Cyb3s@users.noreply.github.com>
2025-03-12 16:02:14 +0100

    Update Dati.java
    aggiunti commenti in dati

--- src/dati/Dati.java
+++ src/dati/Dati.java
@@ -5,5 +5,6 @@ import persona.Persona;
 import java.util.ArrayList;
 
 public class Dati {
+    //creao l'array statico dove andro a gestire le informazioni
     public static ArrayList<Persona> clienti = new ArrayList<Persona>();
 }
