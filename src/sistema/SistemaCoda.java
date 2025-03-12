257cfb78b074f84ef755282a251cd88b54b8bfa9
landi_al <alessandro@landiweb.it>
2025-03-12 15:27:00 +0100

    Aggiunta classe coda

---
+++ src/sistema/SistemaCoda.java
@@ -0,0 +1,14 @@
+package sistema;
+
+import persona.Persona;
+
+public final class SistemaCoda {
+
+    public static void showCurrentlyServing(){
+
+    }
+
+    public static void nextCustomer(){
+
+    }
+}
